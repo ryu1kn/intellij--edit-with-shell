34c426e9757276dad70001b811d3e03ba560e49a
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-08-30 22:04:04 +1000

    Removed java plugin (as we dont need it)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
-    // Java support
-    id("java")
     // Kotlin support
     id("org.jetbrains.kotlin.jvm") version "1.5.30"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
