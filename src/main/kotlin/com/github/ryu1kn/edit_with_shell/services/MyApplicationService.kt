4930a21919ca7401f015700835c7c19aa426f1ae
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2024-05-11 00:26:04 +1000

    Converted services into light services

--- src/main/kotlin/com/github/ryu1kn/edit_with_shell/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ryu1kn/edit_with_shell/services/MyApplicationService.kt
@@ -1,7 +1,9 @@
 package com.github.ryu1kn.edit_with_shell.services
 
 import com.github.ryu1kn.edit_with_shell.MyBundle
+import com.intellij.openapi.components.Service
 
+@Service
 class MyApplicationService {
 
     init {
--- src/main/kotlin/com/github/ryu1kn/edit_with_shell/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ryu1kn/edit_with_shell/services/MyProjectService.kt
@@ -1,8 +1,10 @@
 package com.github.ryu1kn.edit_with_shell.services
 
 import com.github.ryu1kn.edit_with_shell.MyBundle
+import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
 
+@Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
 
     init {
