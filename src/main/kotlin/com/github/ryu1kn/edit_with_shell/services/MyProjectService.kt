477111562e7274feba007b7ca3640ff513b2eef6
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2024-05-13 20:32:22 +1000

    Deleted unnecessary code given by scaffolding

--- src/main/kotlin/com/github/ryu1kn/edit_with_shell/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ryu1kn/edit_with_shell/services/MyProjectService.kt
@@ -1,6 +1,5 @@
 package com.github.ryu1kn.edit_with_shell.services
 
-import com.github.ryu1kn.edit_with_shell.MyBundle
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
 
@@ -8,7 +7,7 @@ import com.intellij.openapi.project.Project
 class MyProjectService(project: Project) {
 
     init {
-        println(MyBundle.message("projectService", project.name))
+        println("projectService ${project.name}")
     }
 
     fun doSomething() {
